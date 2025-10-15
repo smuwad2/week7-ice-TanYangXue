fef577ea0edf69d829e8f4ae3eee5e2d7eb286ff
TanYangXue <tanyangxue@gmail.com>
2025-10-15 09:20:35 +0000

    Edited Exercise 1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,6 +2,47 @@
     export default { 
         // Add Code Here to complete the task
         // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+        data () {
+            return {
+                operators: ["+", "-", "*", "/", "%"],
+                x: 0,
+                y: 0,
+                selectedOp: "+"
+            }
+        },
+        computed: {
+            result () {
+                    if (this.selectedOp == "+") {
+                        return this.x + this.y;
+                    }
+                    if (this.selectedOp == "-") {
+                        return this.x - this.y;
+                    }
+                    if (this.selectedOp == "*") {
+                        return this.x * this.y;
+                    }
+                    if (this.selectedOp == "/") {
+                        if (this.y == 0) {
+                            return "error"
+                        }
+                        return this.x / this.y;
+                    }
+                    if (this.selectedOp == "%") {
+    
+                        if (this.y == 0) {
+                            return "error"
+                        }
+                        return this.x % this.y;
+                    }
+                    else {
+                        return 0
+                    }
+                }     
+            }
+
+                
+        
+        
     }
 </script>
 
