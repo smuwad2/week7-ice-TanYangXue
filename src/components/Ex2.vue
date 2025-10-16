4bd1da541a1e9b29b909d68a9eba7b0c42a93799
TanYangXue <tanyangxue@gmail.com>
2025-10-16 07:04:35 +0000

    Completed Exercise 2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,12 +1,26 @@
 <script>
+
     export default { 
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: "Candy",
             }
-            
+        },
+
+        methods: {
+            deleteParent(index) {
+                this.items.splice(index, 1);
+            },
+            addItem() {
+                const itemName = this.newItem;
+                this.items.push(itemName);
+            }
         }
+
+        
+
     }
 </script>
 
@@ -14,5 +28,11 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul id="itemList">
+        <li v-for="item, index in items" :key="index">
+            {{ item }}<button v-on:click="deleteParent(index)">Delete!</button>
+        </li>
+     </ul>
+     <input type="text" v-model="newItem"></input><button v-on:click="addItem">Add!</button>
    
 </template>
